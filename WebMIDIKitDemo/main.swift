e31d3b3643169c587246f5feeabbb0dc85310d35
Adam Nemecek <adamnemecek@gmail.com>
2017-02-07 12:14:09 -0800

    Initial Commit

---
+++ WebMIDIKitDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WebMIDIKitDemo
+//
+//  Created by Adam Nemecek on 2/7/17.
+//  Copyright © 2017 Adam Nemecek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
