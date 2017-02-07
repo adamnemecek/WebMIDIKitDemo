31514515c499fcbb65f1bd702a7066b22b971ab8
Adam Nemecek <adamnemecek@gmail.com>
2017-02-07 13:44:44 -0800

    stuff

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This project is a simple project demonstrating the use of the [WebMIDIKit framework](https://github.com/adamnemecek/WebMIDIKit/).
 
 Check out the link above for further info.
+
+To use, clone the repo, `cd` into the directory and run `swift package update`. Then open the Xcode project.
+
--- WebMIDIKitDemo/main.swift
+++ WebMIDIKitDemo/main.swift
@@ -6,7 +6,22 @@
 //  Copyright © 2017 Adam Nemecek. All rights reserved.
 //
 
-import Foundation
+import WebMIDIKit
 
-print("Hello, World!")
+let midi = MIDIAccess()
+
+let noteOn: [UInt8] = [0x90, 0x40, 0x7f]
+let noteOff: [UInt8] = [0x80, 0x40, 0]
+
+let input = midi.inputs.prompt()
+
+let output = input.flatMap { midi.output(for: $0) }
+
+input?.onMIDIMessage = { packet in
+  print("received \(packet)")
+}
+
+
+output?.send(noteOn)
+       .send(noteOff, offset: 1000)
 
